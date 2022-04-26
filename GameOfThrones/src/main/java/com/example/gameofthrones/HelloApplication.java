8bfbcdc0493d226c4455f07c0d53af5d1c4d8704
ShohanNur <shohanur.rahman133883@gmail.com>
2022-04-26 21:08:11 +0600

    Game Menu added

--- GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
