3ede131a680eadc70049a330c86731b678fe526d
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-27 23:06:28 +0600

    Update Maze.java
    Now game Scene can be in any rectangle from.

--- GameOfThrones/src/main/java/com/example/gameofthrones/Maze.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/Maze.java
@@ -1,4 +1,5 @@
 package com.example.gameofthrones;
+import com.example.leraringjavafx.Brick;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,6 +8,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane ;
 import javafx.util.Duration;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.net.URL;
@@ -15,16 +17,21 @@ import java.util.ResourceBundle;
 public class Maze implements Initializable {
     @FXML
     AnchorPane maze;
-    private int screenSize = 600;
-    private int row = 6;
-    private int squareLength = screenSize / row;
+    private int screenWidth = 1200;
+    private int screenHeight = 700;
 
-    private int playerRow=5;
-    private  int playerCol=3;
+    private int row = 7;
+    private int col = 12;
+
+    private int rectangleWidth = screenWidth/col;
+    private int rectangleHeight = screenHeight/row;
+
+    private int playerRow=3;
+    private  int playerCol=0;
     private TranslateTransition translate;
     private ImageView player;
     private int[][] isPath;
-    private int[][] visited = new int[row][row];
+    private int[][] visited = new int[row][col];
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -32,15 +39,15 @@ public class Maze implements Initializable {
         init();
 
         player = new ImageView();
-        player.setFitWidth(100);
-        player.setFitHeight(100);
-        player.setY(playerRow*squareLength);
-        player.setX(playerCol*squareLength);
+        player.setFitWidth(rectangleWidth);
+        player.setFitHeight(rectangleHeight);
+        player.setY(playerRow*rectangleHeight);
+        player.setX(playerCol*rectangleWidth);
         player.setImage(new Image(String.valueOf(getClass().getResource("deneris.jpeg"))));
 
         maze.getChildren().add(player);
-        maze.setPrefHeight(screenSize);
-        maze.setPrefWidth(screenSize);
+        maze.setPrefHeight(screenHeight);
+        maze.setPrefWidth(screenWidth);
 
         translate = new TranslateTransition();
         translate.setNode(player);
@@ -49,14 +56,15 @@ public class Maze implements Initializable {
 
         int gridX, gridY;
 
-        for (int i = 0; i < screenSize; i += squareLength)
-            for (int j = 0; j < screenSize; j += squareLength) {
-                gridX = i / squareLength;
-                gridY = j / squareLength;
+        for (int i = 0; i < screenHeight; i += rectangleHeight)
+            for (int j = 0; j < screenWidth; j += rectangleWidth) {
+                gridX = i / rectangleWidth;
+                gridY = j / rectangleHeight;
+                System.out.println(gridX +" "+gridY);
                 if (isPath[gridX][gridY] == 0) {
                     ImageView imageView = new ImageView();
                     Image image = new Image(String.valueOf(getClass().getResource("brick.png")));
-                    com.example.leraringjavafx.Brick brick = new com.example.leraringjavafx.Brick(j, i, squareLength, image, imageView);
+                    Brick brick = new Brick(j, i, rectangleHeight,rectangleWidth,image, imageView);
                     maze.getChildren().add(imageView);
                     brick.draw();
                 }
@@ -65,38 +73,40 @@ public class Maze implements Initializable {
     }
 
     void init() {
-        isPath = new int[row][row];
+        isPath = new int[row][col];
 
         FileReader reader;
 
         int random=1;
-        String path = "path"+Integer.toString(random)+".txt";
 
+        String path = "src\\main\\resources\\com\\example\\gameofthrones\\path"+ Integer.toString(random)+".txt";
 
         try
         {
             reader = new FileReader(path);
-            MakePath makePath = new MakePath(isPath,reader,row,row);
+            MakePath makePath = new MakePath(isPath,reader,row,col);
 
         } catch (FileNotFoundException e) {
-            System.out.println("hi There");
+            System.out.println(e.toString());
         }
 
         for (int gridX = 0; gridX < row; gridX++)
-            for (int gridY = 0; gridY < row; gridY++) {
+            for (int gridY = 0; gridY < col; gridY++) {
                 if(isPath[gridX][gridY]==1) visited[gridX][gridY]=0;
                 else visited[gridX][gridY] = 1;
             }
     }
 
 
+
+    // Moving Main Character
     public void moveUp()
     {
         if(playerRow-1 >=0 && visited[playerRow-1][playerCol]==0)
         {
             playerRow-=1;
             visited[playerRow][playerCol]=1;
-            translate.setByY(-squareLength);
+            translate.setByY(-rectangleHeight);
             translate.setByX(0);
             translate.play();
         }
@@ -107,18 +117,18 @@ public class Maze implements Initializable {
         {
             playerRow+=1;
             visited[playerRow][playerCol]=1;
-            translate.setByY(squareLength);
+            translate.setByY(rectangleHeight);
             translate.setByX(0);
             translate.play();
         }
     }
     public void moveRight()
     {
-        if(playerCol+1<row && visited[playerRow][playerCol+1]==0)
+        if(playerCol+1<col && visited[playerRow][playerCol+1]==0)
         {
             playerCol+=1;
             visited[playerRow][playerCol]=1;
-            translate.setByX(squareLength);
+            translate.setByX(rectangleWidth);
             translate.setByY(0);
             translate.play();
         }
@@ -129,7 +139,7 @@ public class Maze implements Initializable {
         {
             playerCol-=1;
             visited[playerRow][playerCol]=1;
-            translate.setByX(-squareLength);
+            translate.setByX(-rectangleWidth);
             translate.setByY(0);
             translate.play();
         }
