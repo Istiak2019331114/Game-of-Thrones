2541aff04544cd380670c7feddc6027dd2e5a5d3
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-28 14:06:40 +0600

    Add RedSoldier.java

---
+++ GameOfThrones/src/main/java/com/example/gameofthrones/RedSoldier.java
@@ -0,0 +1,26 @@
+package com.example.gameofthrones;
+
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class RedSoldier extends GameElement {
+
+
+    public RedSoldier(int x, int y, int height, int width, AnchorPane pane) {
+        super(x, y, height, width, pane);
+    }
+
+    @Override
+    public void draw()
+    {
+        try
+        {
+            image = new Image(new FileInputStream("src\\main\\resources\\com\\example\\gameofthrones\\redSoldier.png"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
+}
