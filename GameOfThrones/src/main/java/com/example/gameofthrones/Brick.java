81eb477c740759f005ee27c8aeb86fc1682d3835
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-28 14:04:47 +0600

    Update Brick.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/Brick.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/Brick.java
@@ -1,40 +1,26 @@
-package com.example.leraringjavafx;
+package com.example.gameofthrones;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+public class Brick extends GameElement {
 
-public class Brick {
-    private int x;
-    private int y;
-    private int height;
-    private int width;
-    private Image image;
-    private ImageView imageview;
-
-    public Brick(int x, int y, int height,int width, Image image, ImageView imageview) {
-        this.x = x;
-        this.y = y;
-        this.height = height;
-        this.width = width;
-        this.image = image;
-        this.imageview = imageview;
+    public Brick(int x, int y, int height, int width, AnchorPane pane) {
+        super(x, y, height, width, pane);
     }
-
-
+    @Override
     public void draw()
     {
-        imageview.setX(x);
-        imageview.setY(y);
-        imageview.setFitHeight(height);
-        imageview.setFitWidth(width);
-        imageview.setImage(image);
+        try
+        {
+            image = new Image(new FileInputStream("src\\main\\resources\\com\\example\\gameofthrones\\brick.png"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
     }
+
 }
 
 
