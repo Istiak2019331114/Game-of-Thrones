b5f2d05bf010af5ae93efbc3f77b03f8727a64da
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-29 23:49:32 +0600

    Update GameElement.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/GameElement.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/GameElement.java
@@ -4,7 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-abstract public class GameElement {
+public abstract class GameElement {
    private  int x;
    private int y;
    private int height;
