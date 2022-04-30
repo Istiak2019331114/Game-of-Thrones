2634610f7af138a0198f55dcaadc9e39f4d1ddfa
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-30 13:56:19 +0600

    Update HelloApplication.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
@@ -1,8 +1,8 @@
 package com.example.gameofthrones;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,29 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("level.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(new AnchorPane());
 
-        Level controler = fxmlLoader.getController();
-        scene.setOnKeyPressed(keyEvent -> {
-            System.out.println(keyEvent.getCode());
-            switch (keyEvent.getCode())
-            {
-                case W:
-                    controler.moveUp();
-                    break;
-                case S:
-                    controler.moveDown();
-                    break;
-                case A:
-                    controler.moveLeft();
-                    break;
-                case D:
-                    controler.moveRight();
-                    break;
-                default: break;
-            }
-        });
+        Level level = new Level(scene);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
