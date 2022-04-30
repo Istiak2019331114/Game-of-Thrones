e673e0abf0ac01cd46555c981f81d16f858d2708
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-04-30 13:59:23 +0600

    Update LevelThree.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/LevelThree.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/LevelThree.java
@@ -2,5 +2,10 @@ package com.example.gameofthrones;
 
 import javafx.scene.Scene;
 
+import java.io.IOException;
+
 public class LevelThree extends Level{
+    public LevelThree(Scene scene) {
+        super(scene);
+    }
 }
