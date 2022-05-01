ad59d956b86671f8fbf6709605b7a54b7eb875f3
Md. Shohanur Rahman <66036096+ShohanNur@users.noreply.github.com>
2022-05-01 21:02:29 +0600

    sidePanel.java added

---
+++ GameOfThrones/src/main/java/com/example/gameofthrones/sidePanel.java
@@ -0,0 +1,32 @@
+package com.example.gameofthrones;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class sidePanel implements Initializable {
+
+    @FXML
+    private AnchorPane powerShow;
+    @FXML
+    private TextArea playerCurrentPower,redSoldierKillCounts,blackSoldierKillerCounts;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        powerShow.setStyle("-fx-background-position: center center; " +
+                "-fx-background-color:transparent;");
+    }
+    public void PlayerPower(String s){
+        playerCurrentPower.setText(s);
+    }
+    public void RedSoliderKill(String s){
+        redSoldierKillCounts.setText(s);
+    }
+    public void BlackSoldierKill(String s){
+        blackSoldierKillerCounts.setText(s);
+    }
+
+}
