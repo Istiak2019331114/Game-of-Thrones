066a2d263c95ba94eb80d5548d39557d1c31997c
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-01 14:33:53 +0600

    Update GameElement.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/GameElement.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/GameElement.java
@@ -1,18 +1,26 @@
 package com.example.gameofthrones;
 
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+
 
 public abstract class GameElement {
    private  int x;
+   private int labelHeight=20;
    private int y;
    private int height;
    private int width;
+   private int compress=20;
    private AnchorPane pane;
    private int power;
+   public Label powerLabel;
    private ImageView imageView;
    public Image image;
+
    // For Tree
    public GameElement(int x, int y, int height, int width, AnchorPane pane) {
       this.x = x;
@@ -25,12 +33,13 @@ public abstract class GameElement {
    // For Soldiers
    public GameElement(int x, int y, int height, int width, AnchorPane pane,int power) {
       this.x = x;
-      this.y = y;
-      this.height = height;
-      this.width = width;
+      this.y = y+labelHeight;
+      this.height = height-labelHeight;
+      this.width = width-compress;
       this.pane= pane;
       this.power= power;
       setImageView();
+      setPowerLabel(power);
    }
 
 
@@ -47,6 +56,29 @@ public abstract class GameElement {
       imageView.setFitWidth(width);
 
       pane.getChildren().add(imageView);
+   }
+   public void setPowerLabel(int power)
+   {
+      powerLabel = new Label();
+      powerLabel.setText(Integer.toString(power));
+
+      setStyleToPowerLabelText();
+
+      powerLabel.setPrefHeight(labelHeight);
+      powerLabel.setPrefWidth(width+compress);
+      powerLabel.setLayoutX(x);
+      powerLabel.setLayoutY(y-labelHeight);
+      pane.getChildren().add(powerLabel);
+   }
+
+   public  void setStyleToPowerLabelText()
+   {
+      powerLabel.setStyle("-fx-background-position: center center; " +
+              "-fx-background-color:transparent;");
+
+      powerLabel.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
+      powerLabel.setTextFill(Color.CYAN);
+      powerLabel.setTextAlignment(TextAlignment.CENTER);
    }
 
    abstract public void draw();
