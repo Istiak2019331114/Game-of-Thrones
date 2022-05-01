680b63e7da2f9555e960c871b4385ea74e9fb31f
Md. Shohanur Rahman <66036096+ShohanNur@users.noreply.github.com>
2022-05-01 13:24:48 +0600

    Update LevelThree.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/LevelThree.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/LevelThree.java
@@ -1,11 +1,12 @@
 package com.example.gameofthrones;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 
 import java.io.IOException;
 
 public class LevelThree extends Level{
-    public LevelThree(Scene scene) {
-        super(scene);
+    public LevelThree(Scene scene, Group prevroot) {
+        super(scene,prevroot);
     }
 }
