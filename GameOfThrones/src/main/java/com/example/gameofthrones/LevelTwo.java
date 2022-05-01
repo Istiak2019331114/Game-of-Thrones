cb1632ff21ae78131125617ff0d26c4d22e6a305
Md. Shohanur Rahman <66036096+ShohanNur@users.noreply.github.com>
2022-05-01 13:24:17 +0600

    Update LevelTwo.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/LevelTwo.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/LevelTwo.java
@@ -1,10 +1,11 @@
 package com.example.gameofthrones;
 
+import javafx.scene.Group;
 import javafx.scene.Scene;
 
 
 public class LevelTwo extends Level{
-    public LevelTwo(Scene scene) {
-        super(scene);
+    public LevelTwo(Scene scene, Group prevroot) {
+        super(scene,prevroot);
     }
 }
