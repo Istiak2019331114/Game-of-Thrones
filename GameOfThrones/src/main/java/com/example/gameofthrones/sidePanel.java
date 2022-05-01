b2ee1e445b9850207ca3cd7f504028a1c1e8451a
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-01 21:45:39 +0600

    Update sidePanel.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/sidePanel.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/sidePanel.java
@@ -8,15 +8,21 @@ import javafx.scene.layout.AnchorPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class sidePanel implements Initializable {
+public class SidePanel implements Initializable {
 
     @FXML
-    private AnchorPane powerShow;
+    private AnchorPane sidePanel;
     @FXML
     private TextArea playerCurrentPower,redSoldierKillCounts,blackSoldierKillerCounts;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        powerShow.setStyle("-fx-background-position: center center; " +
+        sidePanel.setStyle("-fx-background-position: center center; " +
+                "-fx-background-color:transparent;");
+        playerCurrentPower.setStyle("-fx-background-position: center center; " +
+                "-fx-background-color:transparent;");
+        redSoldierKillCounts.setStyle("-fx-background-position: center center; " +
+                "-fx-background-color:transparent;");
+        blackSoldierKillerCounts.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
     }
     public void PlayerPower(String s){
@@ -27,6 +33,11 @@ public class sidePanel implements Initializable {
     }
     public void BlackSoldierKill(String s){
         blackSoldierKillerCounts.setText(s);
+    }
+    public void setPlace(int x, int y)
+    {
+        sidePanel.setTranslateX(x);
+        sidePanel.setTranslateY(y);
     }
 
 }
