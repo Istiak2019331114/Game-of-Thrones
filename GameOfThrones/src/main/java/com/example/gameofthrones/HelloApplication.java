7feeb0f88a7061f108b9613f91aa3b0c2a601b55
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-01 14:35:22 +0600

    Update HelloApplication.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.gameofthrones;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -11,8 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         Scene scene = new Scene(new AnchorPane());
+        Group group = new Group();
 
-        Level level = new Level(scene);
+        Level level = new Level(scene,group);
 
         stage.setTitle("Hello!");
         stage.setScene(scene);
