e6d585fce73857aa42b98372048ae9cef5224b71
Md. Shohanur Rahman <66036096+ShohanNur@users.noreply.github.com>
2022-05-02 15:04:50 +0600

    Update SidePanel.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/SidePanel.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/SidePanel.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -14,26 +15,35 @@ public class SidePanel implements Initializable {
     @FXML
     private AnchorPane sidePanel;
     @FXML
-    private Label playerCurrentPower,redSoldierKilledCounts,blackSoldierKilledCounts;
+    private Label playerCurrentPower,KillCounts,currentLevel;
+    @FXML
+    private ImageView currentHouseLogo;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         sidePanel.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
         playerCurrentPower.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
-        redSoldierKilledCounts.setStyle("-fx-background-position: center center; " +
+        KillCounts.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
-        blackSoldierKilledCounts.setStyle("-fx-background-position: center center; " +
+        currentLevel.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
+        currentHouseLogo.setStyle("-fx-background-position: center center; " +
+                "-fx-background-color:transparent;");
+
     }
     public void PlayerPower(String s){
         playerCurrentPower.setText(s);
     }
-    public void RedSoliderKill(String s){
-        redSoldierKilledCounts.setText(s);
+    public void TotalKill(String s){
+        KillCounts.setText(s);
     }
-    public void BlackSoldierKill(String s){
-        blackSoldierKilledCounts.setText(s);
+    public void curLevel(String s) {currentLevel.setText(s);}
+    public void curHouseLogo(String img){
+
+        currentHouseLogo.setStyle("-fx-background-image: url('" + img + "'); " +
+                "-fx-background-position: center center; " +
+                "-fx-background-size: 100% 100%;");
     }
     public void setPlace(int x, int y)
     {
