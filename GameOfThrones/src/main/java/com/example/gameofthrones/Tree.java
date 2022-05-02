38cc71c87511c5d483a08e0b9c30fba44294953f
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-02 13:11:34 +0600

    Update Tree.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/Tree.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/Tree.java
@@ -7,15 +7,16 @@ import java.io.FileNotFoundException;
 
 public class Tree extends GameElement {
 
-    public Tree(int x, int y, int height, int width, AnchorPane pane) {
-        super(x, y, height, width, pane);
+    public Tree(int x, int y, int height, int width, AnchorPane pane,String treeName) {
+        super(x, y, height, width, pane,treeName);
     }
     @Override
     public void draw()
     {
+        String path =  "src\\main\\resources\\com\\example\\gameofthrones\\"+getImageName();
         try
         {
-            image = new Image(new FileInputStream("src\\main\\resources\\com\\example\\gameofthrones\\appleTree.png"));
+            image = new Image(new FileInputStream(path));
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
