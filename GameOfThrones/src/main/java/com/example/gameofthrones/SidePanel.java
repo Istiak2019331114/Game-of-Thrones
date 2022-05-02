2102243f469d716a6c83a2f269cbb93e3f4c60b5
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-02 16:29:23 +0600

    Update SidePanel.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/SidePanel.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/SidePanel.java
@@ -4,9 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,6 +19,7 @@ public class SidePanel implements Initializable {
     private AnchorPane sidePanel;
     @FXML
     private Label playerCurrentPower,KillCounts,currentLevel;
+
     @FXML
     private ImageView currentHouseLogo;
     @Override
@@ -28,22 +32,27 @@ public class SidePanel implements Initializable {
                 "-fx-background-color:transparent;");
         currentLevel.setStyle("-fx-background-position: center center; " +
                 "-fx-background-color:transparent;");
-        currentHouseLogo.setStyle("-fx-background-position: center center; " +
-                "-fx-background-color:transparent;");
-
     }
-    public void PlayerPower(String s){
-        playerCurrentPower.setText(s);
+    public void setPlayerPower(int power){
+        playerCurrentPower.setText(Integer.toString(power));
     }
-    public void TotalKill(String s){
-        KillCounts.setText(s);
+    public void setKillCount(int killCount){
+        KillCounts.setText(Integer.toString(killCount));
     }
-    public void curLevel(String s) {currentLevel.setText(s);}
-    public void curHouseLogo(String img){
-
-        currentHouseLogo.setStyle("-fx-background-image: url('" + img + "'); " +
-                "-fx-background-position: center center; " +
-                "-fx-background-size: 100% 100%;");
+    public void setCurrentLevel(String levelName) {
+        currentLevel.setText(levelName);
+        System.out.println(levelName);
+    }
+    public void setCurrentHouseLogo(String imageName){
+        String path =  "src\\main\\resources\\com\\example\\gameofthrones\\"+imageName;
+        Image image=null;
+        try
+        {
+            image = new Image(new FileInputStream(path));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+        currentHouseLogo.setImage(image);
     }
     public void setPlace(int x, int y)
     {
