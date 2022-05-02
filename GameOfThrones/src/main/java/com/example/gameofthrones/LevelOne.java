90259edf0cb48445648bb2012fa6ec09b6f9c75f
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-05-02 13:08:19 +0600

    Update LevelOne.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/LevelOne.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/LevelOne.java
@@ -1,6 +1,5 @@
 package com.example.gameofthrones;
 
-import com.almasb.fxgl.physics.box2d.dynamics.joints.LimitState;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.util.Pair;
@@ -13,72 +12,112 @@ public class LevelOne extends Level{
     private int numOfBlackSoldier;
     private int numOfRedSoldier;
     private int numOfTent;
-    private final int maxRedSoldier=15;
+    private final int maxNumOfRedSoldier =15;
+    private final int minNumOfRedSoldier =10;
     private final int maxNumOfTent=5;
+    private final int minNumOfTent=3;
+
     private int blackSoldierMaxPower=105;
     private int redSoldierMaxPower=80;
-
+    private int increment=20;
+    private int basePower=80;
+    private int[] basePowerAtCol;
+    private List<Pair<Integer,Integer>> availableCell;
     public LevelOne(Scene scene, Group prevroot) {
         super(scene,prevroot);
         setSoldiers();
     }
 
+    @Override
+    public void setTheme() {
+       setTreeName("levelOneTree.png");
+       setBackgroundName("levelOneBG.jpg");
+    }
     private void setSoldiers()
     {
-        numOfRedSoldier= getRandom()%maxRedSoldier;
-        numOfTent= getRandom()%maxNumOfTent;
-        numOfBlackSoldier-=(numOfRedSoldier+numOfTent);
-        List<Pair<Integer,Integer>> pairs =  new ArrayList<Pair<Integer,Integer>>();
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-            {
-                if(!(i==getPlayerCellPaneRow() && j==getPlayerCellPaneCol()) && isPath[i][j]==1)
-                {
-                    pairs.add(new Pair<Integer,Integer>(i,j));
-                }
-            }
-        int numOfPairs=pairs.size();
+
+        setNumberofSoldier();
+
+        getAvailableCell();
+
+        setBasePower();
+
+        int numOfPairs= availableCell.size();
+
         int index;
         int curRow, curCol;
+        int soldierPower;
+
         Pair<Integer, Integer> curCell = new Pair<Integer, Integer>(0,0);
+
+        GameElement soldier,tent;
+
         while (numOfPairs>0)
         {
+            // selecting index randomly
             index=getRandom()%numOfPairs;
-            curCell= pairs.get(index);
+            curCell= availableCell.get(index);
 
-            pairs.remove(index);
+            availableCell.remove(index);
             numOfPairs--;
 
             curRow =curCell.getKey();
             curCol=curCell.getValue();
-            GameElement soldier;
+
+            soldierPower= basePowerAtCol[curCol]+(getRandom())%increment;
+
             if(numOfTent>0)
             {
 
                 if(getRandom()%2==0)
                 {
-                    soldier =new BlackSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,getRandom()%blackSoldierMaxPower);
+                    soldier =new BlackSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldierPower);
                 }
                 else {
-                    soldier =new RedSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,getRandom()%redSoldierMaxPower);
+                    soldier =new RedSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldierPower);
 
                 }
-                Tent tent =new Tent(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldier);
+                tent =new Tent(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldier);
                 gameElements[curRow][curCol]=tent;
                 numOfTent--;
             }
+
             else if(numOfRedSoldier>0)
             {
-                soldier =new RedSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,getRandom()%redSoldierMaxPower);
+                soldier =new RedSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldierPower);
                 gameElements[curRow][curCol]=soldier;
                 numOfRedSoldier--;
             }
             else {
-                soldier =new BlackSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,getRandom()%blackSoldierMaxPower);
+                soldier =new BlackSoldier(curCol*rectangleWidth,curRow*rectangleHeight,rectangleHeight,rectangleWidth,maze,soldierPower);
                 gameElements[curRow][curCol]=soldier;
                 numOfBlackSoldier--;
             }
         }
+    }
+    private void setNumberofSoldier()
+    {
+        numOfRedSoldier= minNumOfRedSoldier + getRandom()%(maxNumOfRedSoldier - minNumOfRedSoldier);
+        numOfTent=minNumOfTent+ getRandom()%(maxNumOfTent-minNumOfTent);
+        numOfBlackSoldier=getTotalSoldier()-numOfTent-numOfRedSoldier;
+    }
+    private void getAvailableCell()
+    {
+        availableCell =  new ArrayList<Pair<Integer,Integer>>();
+
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+            {
+                if(!(i==getPlayerCellPaneRow() && j==getPlayerCellPaneCol()) && isPath[i][j]==1)
+                {
+                    availableCell.add(new Pair<Integer,Integer>(i,j));
+                }
+            }
+    }
+    private void setBasePower()
+    {
+        basePowerAtCol = new int [col];
+        for(int i=0;i<col;i++) basePowerAtCol[i]=basePower+(increment*i);
     }
     private int getRandom()
     {
