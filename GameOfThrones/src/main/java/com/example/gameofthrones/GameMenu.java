9f9d285df9746c77141a26c829fe21c527bcd107
Md. Shohanur Rahman <66036096+ShohanNur@users.noreply.github.com>
2022-05-02 15:07:49 +0600

    Update GameMenu.java

--- GameOfThrones/src/main/java/com/example/gameofthrones/GameMenu.java
+++ GameOfThrones/src/main/java/com/example/gameofthrones/GameMenu.java
@@ -31,7 +31,7 @@ import java.nio.file.Paths;
 import static javafx.application.Application.launch;
 
 public class GameMenu extends Application {
-    private GMenu gameMenu;
+    private GameMenuItems gameMenuItems;
     private Scene scene;
     private Group root;
     private int sceneWidth =1330;
@@ -47,29 +47,29 @@ public class GameMenu extends Application {
             Image img = new Image(path);
             path.close();
 
-            ImageView imgView = new ImageView(img);
-            imgView.setFitWidth(sceneWidth);
-            imgView.setFitHeight(sceneHeight);
-            gameMenu = new GMenu();
-            gameMenu.setVisible(false);
-            root.getChildren().addAll(imgView,gameMenu);
+            ImageView backGroundImgView = new ImageView(img);
+            backGroundImgView.setFitWidth(sceneWidth);
+            backGroundImgView.setFitHeight(sceneHeight);
+            gameMenuItems = new GameMenuItems();
+            gameMenuItems.setVisible(false);
+            root.getChildren().addAll(backGroundImgView, gameMenuItems);
 
             scene = new Scene(root,sceneWidth,sceneHeight);
             scene.setOnKeyPressed(event->{
                 if(event.getCode()== KeyCode.ESCAPE){
-                    if(!gameMenu.isVisible()){
-                        FadeTransition ft = new FadeTransition(Duration.seconds(0.5),gameMenu);
-                        ft.setFromValue(0);
-                        ft.setToValue(1);
-                        gameMenu.setVisible(true);
-                        ft.play();
+                    if(!gameMenuItems.isVisible()){
+                        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), gameMenuItems);
+                        fadeTransition.setFromValue(0);
+                        fadeTransition.setToValue(1);
+                        gameMenuItems.setVisible(true);
+                        fadeTransition.play();
                     }
                     else{
-                        FadeTransition ft = new FadeTransition(Duration.seconds(0.5),gameMenu);
-                        ft.setFromValue(1);
-                        ft.setToValue(0);
-                        ft.setOnFinished(evt -> gameMenu.setVisible(false));
-                        ft.play();
+                        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5), gameMenuItems);
+                        fadeTransition.setFromValue(1);
+                        fadeTransition.setToValue(0);
+                        fadeTransition.setOnFinished(evt -> gameMenuItems.setVisible(false));
+                        fadeTransition.play();
                     }
                 }
             });
@@ -89,60 +89,60 @@ public class GameMenu extends Application {
         mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
         mediaPlayer.play();
     }
-    private class GMenu extends Parent {
-        public GMenu(){
-            VBox menu0 = new VBox(10);
-            VBox menu1 = new VBox(10);
-            VBox menu2 = new VBox(10);
+    private class GameMenuItems extends Parent {
+        public GameMenuItems(){
+            VBox homeMenu = new VBox(10);
+            VBox optionsMenu = new VBox(10);
+            VBox newGameMenu = new VBox(10);
 
-            menu0.setTranslateX(100);
-            menu0.setTranslateY(200);
-            menu1.setTranslateX(100);
-            menu1.setTranslateY(200);
-            menu2.setTranslateX(100);
-            menu2.setTranslateY(200);
+            homeMenu.setTranslateX(100);
+            homeMenu.setTranslateY(200);
+            optionsMenu.setTranslateX(100);
+            optionsMenu.setTranslateY(200);
+            newGameMenu.setTranslateX(100);
+            newGameMenu.setTranslateY(200);
 
             final int offset = 400;
-            menu1.setTranslateX(offset);
+            optionsMenu.setTranslateX(offset);
             MenuButton btnNewGame = new MenuButton("NEW GAME");
             btnNewGame.setOnMouseClicked(event->{
-                getChildren().add(menu2);
-                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25),menu0);
-                tt.setToX(menu0.getTranslateX() - offset);
+                getChildren().add(newGameMenu);
+                TranslateTransition translate = new TranslateTransition(Duration.seconds(0.25),homeMenu);
+                translate.setToX(homeMenu.getTranslateX() - offset);
 
-                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.50),menu2);
-                tt1.setToX(menu0.getTranslateX());
+                TranslateTransition translate1 = new TranslateTransition(Duration.seconds(0.50),newGameMenu);
+                translate1.setToX(homeMenu.getTranslateX());
 
-                tt.play();
-                tt1.play();
+                translate.play();
+                translate1.play();
 
-                tt.setOnFinished(evt ->{
-                    getChildren().remove(menu0);
+                translate.setOnFinished(evt ->{
+                    getChildren().remove(homeMenu);
                 });
             });
 
             MenuButton btnResum = new MenuButton("RESUME");
             btnResum.setOnMouseClicked(event->{
-                FadeTransition ft = new FadeTransition(Duration.seconds(0.5),this) ;
-                ft.setFromValue(1);
-                ft.setToValue(0);
-                ft.setOnFinished(evt -> setVisible(false));
-                ft.play();
+                FadeTransition fadeTranslate = new FadeTransition(Duration.seconds(0.5),this) ;
+                fadeTranslate.setFromValue(1);
+                fadeTranslate.setToValue(0);
+                fadeTranslate.setOnFinished(evt -> setVisible(false));
+                fadeTranslate.play();
             });
             MenuButton btnOptions = new MenuButton("OPTIONS");
             btnOptions.setOnMouseClicked(event->{
-                getChildren().add(menu1);
-                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25),menu0);
-                tt.setToX(menu0.getTranslateX() - offset);
+                getChildren().add(optionsMenu);
+                TranslateTransition translate = new TranslateTransition(Duration.seconds(0.25),homeMenu);
+                translate.setToX(homeMenu.getTranslateX() - offset);
 
-                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.50),menu1);
-                tt1.setToX(menu0.getTranslateX());
+                TranslateTransition translate1 = new TranslateTransition(Duration.seconds(0.50),optionsMenu);
+                translate1.setToX(homeMenu.getTranslateX());
 
-                tt.play();
-                tt1.play();
+                translate.play();
+                translate1.play();
 
-                tt.setOnFinished(evt ->{
-                    getChildren().remove(menu0);
+                translate.setOnFinished(evt ->{
+                    getChildren().remove(homeMenu);
                 });
             });
             MenuButton btnExit = new MenuButton("EXIT");
@@ -151,34 +151,34 @@ public class GameMenu extends Application {
             });
             MenuButton btnBack = new MenuButton("BACK");
             btnBack.setOnMouseClicked(event->{
-                getChildren().add(menu0);
-                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25),menu1);
-                tt.setToX(menu1.getTranslateX()+offset);
+                getChildren().add(homeMenu);
+                TranslateTransition translate = new TranslateTransition(Duration.seconds(0.25),optionsMenu);
+                translate.setToX(optionsMenu.getTranslateX()+offset);
 
-                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5),menu0);
-                tt1.setToX(menu1.getTranslateX());
+                TranslateTransition translate1 = new TranslateTransition(Duration.seconds(0.5),homeMenu);
+                translate1.setToX(optionsMenu.getTranslateX());
 
-                tt.play();
-                tt1.play();
+                translate.play();
+                translate1.play();
 
-                tt.setOnFinished(evt ->{
-                    getChildren().remove(menu1);
+                translate.setOnFinished(evt ->{
+                    getChildren().remove(optionsMenu);
                 });
             });
             MenuButton btnBackk = new MenuButton("GO BACK");
             btnBackk.setOnMouseClicked(event->{
-                getChildren().add(menu0);
-                TranslateTransition tt = new TranslateTransition(Duration.seconds(0.25),menu2);
-                tt.setToX(menu2.getTranslateX()+offset);
+                getChildren().add(homeMenu);
+                TranslateTransition translate = new TranslateTransition(Duration.seconds(0.25),newGameMenu);
+                translate.setToX(newGameMenu.getTranslateX()+offset);
 
-                TranslateTransition tt1 = new TranslateTransition(Duration.seconds(0.5),menu0);
-                tt1.setToX(menu2.getTranslateX());
+                TranslateTransition translate1 = new TranslateTransition(Duration.seconds(0.5),homeMenu);
+                translate1.setToX(newGameMenu.getTranslateX());
 
-                tt.play();
-                tt1.play();
+                translate.play();
+                translate1.play();
 
-                tt.setOnFinished(evt ->{
-                    getChildren().remove(menu2);
+                translate.setOnFinished(evt ->{
+                    getChildren().remove(newGameMenu);
                 });
             });
 
@@ -191,27 +191,28 @@ public class GameMenu extends Application {
                 mediaPlayer.stop();
                 new Video(scene,root);
             });
-            MenuButton btnHouse1 = new MenuButton("HOUSE LANNISTER");
-            MenuButton btnHouse2 = new MenuButton("HOUSE BARATHEON");
-            MenuButton btnHouse3 = new MenuButton("HOUSE GREYJOY");
+            MenuButton btnHouseLannister = new MenuButton("HOUSE LANNISTER");
+            MenuButton btnHouseBaratheon = new MenuButton("HOUSE BARATHEON");
+            MenuButton btnHouseGreyjoy = new MenuButton("HOUSE GREYJOY");
 
-            btnHouse1.setOnMouseClicked(event->{
+            btnHouseGreyjoy.setOnMouseClicked(event->{
                 new LevelOne(scene,root);
             });
-            btnHouse2.setOnMouseClicked(event->{
-                new LevelTwo(scene,root);
-            });
-            btnHouse3.setOnMouseClicked(event->{
+            btnHouseLannister.setOnMouseClicked(event->{
                 new LevelThree(scene,root);
             });
+            btnHouseBaratheon.setOnMouseClicked(event->{
+                new LevelTwo(scene,root);
+            });
 
-            menu0.getChildren().addAll(btnNewGame,btnResum,btnOptions,btnExit);
-            menu1.getChildren().addAll(btnBack,btnSound,btnVideo);
-            menu2.getChildren().addAll(btnHouse1,btnHouse2,btnHouse3,btnBackk);
+
+            homeMenu.getChildren().addAll(btnNewGame,btnResum,btnOptions,btnExit);
+            optionsMenu.getChildren().addAll(btnBack,btnSound,btnVideo);
+            newGameMenu.getChildren().addAll(btnHouseGreyjoy,btnHouseBaratheon,btnHouseLannister,btnBackk);
             Rectangle bg = new Rectangle(sceneWidth,sceneHeight);
             bg.setFill(Color.GREY);
             bg.setOpacity(0.6);
-            getChildren().addAll(bg,menu0);
+            getChildren().addAll(bg,homeMenu);
         }
     }
     private static class MenuButton extends StackPane{
@@ -220,31 +221,31 @@ public class GameMenu extends Application {
             text = new Text(name);
             text.setFont(text.getFont().font(20));
 
-            Rectangle bg = new Rectangle(250,30);
-            bg.setOpacity(0.7);
-            bg.setFill(Color.BLACK);
-            bg.setEffect(new GaussianBlur(3.5));
+            Rectangle RectbackGround = new Rectangle(250,30);
+            RectbackGround.setOpacity(0.7);
+            RectbackGround.setFill(Color.BLACK);
+            RectbackGround.setEffect(new GaussianBlur(3.5));
 
             setAlignment(Pos.CENTER_LEFT);
             setRotate(-0.5);
-            getChildren().addAll(bg,text);
+            getChildren().addAll(RectbackGround,text);
 
             setOnMouseEntered(event->{
-                bg.setTranslateX(10);
+                RectbackGround.setTranslateX(10);
                 text.setTranslateX(10);
-                bg.setFill(Color.WHITE);
+                RectbackGround.setFill(Color.WHITE);
                 text.setFill(Color.BLACK);
             });
             setOnMouseExited(event->{
-                bg.setTranslateX(0);
+                RectbackGround.setTranslateX(0);
                 text.setTranslateX(0);
-                bg.setFill(Color.BLACK);
+                RectbackGround.setFill(Color.BLACK);
                 text.setFill(Color.WHITE);
             });
-            DropShadow drop = new DropShadow(50,Color.WHITE);
-            drop.setInput(new Glow());
+            DropShadow dropShadow = new DropShadow(50,Color.WHITE);
+            dropShadow.setInput(new Glow());
 
-            setOnMousePressed(event-> setEffect(drop));
+            setOnMousePressed(event-> setEffect(dropShadow));
             setOnMouseReleased(event-> setEffect(null));
         }
     }
