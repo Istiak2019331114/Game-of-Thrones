2a168f1ec59602864d35a5e972678176e2d245d3
Md Istiak <69497799+Istiak2019331114@users.noreply.github.com>
2022-08-09 03:21:17 +0600

    Create LevelCount.java

---
+++ GameOfThrones/src/main/java/com/example/gameofthrones/LevelCount.java
@@ -0,0 +1,14 @@
+package com.example.gameofthrones;
+
+public class LevelCount {
+    private int levelCount=0;
+    public void incrementLevelCount()
+    {
+        levelCount++;
+    }
+    public int getLevelCount()
+    {
+      return  levelCount;
+    }
+
+}
